4b3d959d48f1bd9bd8450d2e401d20c2adadd3ad
MiscellaneousStuff <joe.makepeace01@gmail.com>
2022-12-11 23:24:57 +0000

    Added generic test class

---
+++ traffic_ml/tests/utils.py
@@ -0,0 +1,34 @@
+# MIT License
+# 
+# Copyright (c) 2022 MEng-Team-Project
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""Unit testing tools."""
+
+from absl.testing import absltest
+
+
+class TestCase(absltest.TestCase):
+    """A test base class that enables stopwatch profiling."""
+
+    def setUp(self):
+        super(TestCase, self).setup()
+
+    def tearDown(self):
+        super(TestCase, self).tearDown()
